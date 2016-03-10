684fcf88c628705b4b60ac9d6818a1e5041e6125
Nicolas Oliveira <nk.oliveira@gmail.com>
2016-03-10 00:23:41 -0300

    Ajuste menu usuário

--- src/main/webapp/resources/aftermidnight/js/app.js
+++ src/main/webapp/resources/aftermidnight/js/app.js
@@ -1,6 +1,16 @@
 $(function() {
+	
+	/*Layout Functions*/
+	
 	$('.js-toggle').bind('click', function(event) {
 		$('.js-sidebar, .js-content').toggleClass('is-toggled');
 		event.preventDefault();
-	});	
+	});
+
+	$('.js-userinfo-dropdown').bind('click', function(event){
+		$('.js-userinfo-dropdown').toggleClass('selected');
+		$('.js-userinfo-navbar').toggleClass('is-toggled');
+		event.preventDefault();
+	});
+
 });
